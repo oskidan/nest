a1ba52fd7f81b4470bac006b0d0537426c642cb5
Oleksii Skidan <al.skidan@gmail.com>
2017-11-23 16:07:30 +0200

    Initial implementation of vertex traits.

---
+++ nest/vertex_traits.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+namespace nest {
+inline namespace v1 {
+
+/// Holds a boolean value which specifies whether the given type `T` has a field named `position`.
+/// @{
+template <typename T, typename = void>
+static constexpr bool has_position = false;
+
+template <typename T>
+constexpr bool has_position<T, std::void_t<decltype(std::declval<T>().position)>> = true;
+/// @}
+
+/// Holds a boolean value which specifies whether the given type `T` has a field named `color`.
+/// @{
+template <typename T, typename = void>
+static constexpr bool has_color = false;
+
+template <typename T>
+constexpr bool has_color<T, std::void_t<decltype(std::declval<T>().color)>> = true;
+/// @}
+
+/// Holds a boolean value which specifies whether the given type `T` has a field named `texcoord`.
+/// @{
+template <typename T, typename = void>
+static constexpr bool has_texcoord = false;
+
+template <typename T>
+constexpr bool has_texcoord<T, std::void_t<decltype(std::declval<T>().texcoord)>> = true;
+/// @}
+
+} // namespace v1
+} // namespace nest
+
---
+++ test/vertex_traits.cpp
@@ -0,0 +1,65 @@
+#include <cstdlib>
+#include <iostream>
+
+#include <glm/glm.hpp>
+
+#include <nest/vertex_traits.hpp>
+
+struct Vertex {
+    float position[3];
+    glm::vec3 color;
+    glm::vec2 texcoord;
+};
+
+struct Dummy {
+};
+
+struct Evil {
+    int position()
+    {
+        return 0;
+    }
+
+    float color()
+    {
+        return 0.f;
+    }
+
+    bool texcoord()
+    {
+        return false;
+    }
+};
+
+/*
+c++ -std=c++17 -g -O0 -Wall -Werror -I. test/vertex_traits.cpp
+
+Expected output:
+    1 0 0
+    1 0 0
+    1 0 0
+*/
+
+int main(int const argc, char const* const argv[])
+{
+    // clang-format off
+    std::cout << nest::has_position<Vertex> << " " 
+              << nest::has_position<Dummy>  << " "
+              << nest::has_position<Evil>   << std::endl;
+    // clang-format on
+
+    // clang-format off
+    std::cout << nest::has_color<Vertex> << " " 
+              << nest::has_color<Dummy>  << " "
+              << nest::has_color<Evil>   << std::endl;
+    // clang-format on
+
+    // clang-format off
+    std::cout << nest::has_texcoord<Vertex> << " " 
+              << nest::has_texcoord<Dummy>  << " "
+              << nest::has_texcoord<Evil>   << std::endl;
+    // clang-format on
+
+    return EXIT_SUCCESS;
+}
+
